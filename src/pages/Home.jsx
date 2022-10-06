86c202470a3dfb0f2aa0fd12e2d92dd4596e2208
abdelrahman <zeid4643@gmail.com>
2022-10-06 16:16:55 +0200

    confirm

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { Navbar, Main, Product, Footer } from "../components";
+import React, { Component }  from 'react';
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+      <Product />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
